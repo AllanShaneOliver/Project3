7c0723c3ae8cf4481837876b8e81ea06be0f5127
Shane Oliver <shaneoliver@Shanes-Air.fios-router.home>
2020-12-09 00:08:39 -0500

    reads input into string

--- project3.s
+++ project3.s
@@ -4,3 +4,10 @@ invalid: .asciiz "NaN\n"
 
 .text
 .globl main
+
+main:
+  li $s4, 0
+  li $v0, 8 #reads strings into list string
+  la $a0, list
+  li $a1, 5 #input no more than 10 characters
+  syscall
